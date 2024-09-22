3d04e97e2895ab5d684aea3d1415b44315743159
liiiimiiii <li7059819@gmail.com>
2024-09-22 16:59:57 +0800

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
-const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
-const NEZHA_PORT = process.env.NEZHA_PORT || '5555';
-const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.zzx.free.hr';
-
-
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '8xix888xix8.kunei.eu.org';     
+const NEZHA_PORT = process.env.NEZHA_PORT || '443';
+const NEZHA_KEY = process.env.NEZHA_KEY || '111111';
 
 // root route
 app.get("/", function(req, res) {
@@ -15,16 +13,29 @@ app.get("/", function(req, res) {
 });
 
 
+app.use('/xyz', createProxyMiddleware({
+  target: 'http://localhost:8080',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/xyz': '/',
+  }
+}));
+
+const metaInfo = execSync(
+  'curl -s https://speed.cloudflare.com/meta | awk -F\\" \'{print $26"-"$18}\' | sed -e \'s/ /_/g\'',
+  { encoding: 'utf-8' }
+);
+const ISP = metaInfo.trim();
 
 // run-nezha
-  let NEZHA_TLS = '';
-  if (NEZHA_SERVER && NEZHA_PORT && NEZHA_KEY) {
-    const tlsPorts = ['443', '8443', '2096', '2087', '2083', '2053'];
-    if (tlsPorts.includes(NEZHA_PORT)) {
-      NEZHA_TLS = '--tls';
-    } else {
-      NEZHA_TLS = '';
-    }
+let NEZHA_TLS = '';
+if (NEZHA_SERVER && NEZHA_PORT && NEZHA_KEY) {
+  const tlsPorts = ['443', '8443', '2096', '2087', '2083', '2053'];
+  if (tlsPorts.includes(NEZHA_PORT)) {
+    NEZHA_TLS = '--tls';
+  } else {
+    NEZHA_TLS = '';
+  }
   const command = `nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &`;
   try {
     exec(command);
@@ -40,7 +51,6 @@ app.get("/", function(req, res) {
   console.log('NEZHA variable is empty, skip running');
   runWeb();
 }
-
 
 function runWeb() {
   const command1 = `nohup ./web -c ./web.json >/dev/null 2>&1 &`;
